f8b845ce2068cb4ae35b56a7b7ff8e944091b88f
GuillermoCAB <guiga.benitez@gmail.com>
2025-08-24 20:42:25 -0300

    feat: remove comment

--- src/routes/PlanetRoutes.tsx
+++ src/routes/PlanetRoutes.tsx
@@ -2,7 +2,6 @@ import { Route } from "react-router-dom";
 import { PlanetDetail } from "../pages";
 
 const planetRoutes = [
-  // Initial implementation remove in favor of real component
   <Route key="planet-detail" path="/planet/:id" element={<PlanetDetail />} />,
 ];
 
