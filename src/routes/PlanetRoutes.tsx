57625923896ec468d93548ad5559cc36f8e4a407
GuillermoCAB <guiga.benitez@gmail.com>
2025-08-24 20:13:00 -0300

    feat: implement component

--- src/routes/PlanetRoutes.tsx
+++ src/routes/PlanetRoutes.tsx
@@ -1,12 +1,9 @@
 import { Route } from "react-router-dom";
+import { PlanetDetail } from "../pages";
 
 const planetRoutes = [
   // Initial implementation remove in favor of real component
-  <Route
-    key="planet-detail"
-    path="/planet/:id"
-    element={<div>Planet Detail Page</div>}
-  />,
+  <Route key="planet-detail" path="/planet/:id" element={<PlanetDetail />} />,
 ];
 
 export default planetRoutes;
