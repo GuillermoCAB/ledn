60fa3c19fa92fdf945d08c6ddfe9c774956ca341
GuillermoCAB <guiga.benitez@gmail.com>
2025-08-24 18:27:36 -0300

    feat: add exchange rate query hooks [AI]

---
+++ src/hooks/api/exchangeRate.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { ExchangeRate } from "../../types";
+import api from "../../constants/baseApi";
+
+export const useExchangeRate = () => {
+  return useQuery<ExchangeRate>({
+    queryKey: ["exchangeRate"],
+    queryFn: async () => {
+      const { data } = await api.get("/exchange-rate");
+      return data;
+    },
+  });
+};
